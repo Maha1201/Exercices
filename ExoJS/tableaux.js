3976cbf93e96204bc41e768b0e251f39610336bf
Maha1201 <abdalla.m1201@gmail.com>
2022-10-13 16:53:13 +0200

    Exercice 2 presque fini

--- ExoJS/tableaux.js
+++ ExoJS/tableaux.js
@@ -1,54 +1,79 @@
-// Exercice 1 : 
-let tableau = Array(parseInt(prompt("Choisissez la taille du tableau !")));
-let text = "<ul>"; // Permet de créer une liste
+// // Exercice 1 : 
+// let tableau = Array(parseInt(prompt("Choisissez la taille du tableau !")));
+// let text = "<ul>"; // Permet de créer une liste
 
-for(i=0; i<tableau.length; i++){
+// for(i=0; i<tableau.length; i++){
 
-    let remplir = prompt("Entrez la valeur " + i + " du tableau");
-    tableau [i] = [remplir];
-    text += "<li>" + tableau[i] + "</li>"; // Liste les valeurs i du tableau 
-}
-text +="</ul>"; // Fermer la liste
+//     let remplir = prompt("Entrez la valeur " + i + " du tableau");
+//     tableau [i] = [remplir];
+//     text += "<li>" + tableau[i] + "</li>"; // Liste les valeurs i du tableau 
+// }
+// text +="</ul>"; // Fermer la liste
 
-console.log("La taille du tableau est de : " + tableau.length); // Dans la console uniquement
-console.table(tableau);
+// console.log("La taille du tableau est de : " + tableau.length); // Dans la console uniquement
+// console.table(tableau);
 
-document.getElementById("tableauPhrase").innerHTML = ("Les valeurs contenues dans le tableau sont les suivantes : " + tableau);
-document.getElementById("tableauID").innerHTML = text; // Affiche "text" donc le tableau créé dedans
+// document.getElementById("tableauPhrase").innerHTML = ("Les valeurs contenues dans le tableau sont les suivantes : " + tableau);
+// document.getElementById("tableauID").innerHTML = text; // Affiche "text" donc le tableau créé dedans
 
 
 // Exercice 2 :
+let entier;
+let tableau2;
 
-function GetInteger(){
-    let entier = prompt("Entrez un entier");
+function GetInteger(entier){
+    while(entier != parseInt(entier)){
+        entier = prompt("Erreur ! Entrez uniquement un entier !!");
+    }
 }
 
 function InitTab(){
-    let tableau2 = Array(parseInt(prompt("Choisissez la taille du tableau !")));
+    tableau2 = Array(parseInt(prompt("Choisissez la taille du tableau !")));
+    GetInteger(tableau2);
 }
 
 function SaisieTab(){
+    InitTab();
     for(i=0; i<tableau2.length; i++){
-        let remplir2 = prompt("Entrez la valeur " + i + " du tableau");
+        let remplir2 = prompt("Entrez la valeur " + (i+1) + " du tableau");
+        GetInteger(remplir2);
         tableau2 [i] = [remplir2];
     }
 }
 
 function AfficheTab(){
+    SaisieTab();
+    document.getElementById("tableauPhrase2").innerHTML = ("<br> Les valeurs contenues dans le tableau sont les suivantes : " + tableau2);
+   
+    var contenu = "<ul>";
+    for(i=0; i<tableau2.length; i++){ // Boucle pour lister les éléments du tableau
+        contenu += "<li>" + tableau2[i] + "</li>";
+    }
+    contenu += "</ul>";
 
+    document.getElementById("tableauEnSoi").innerHTML = contenu;
 }
 
-function RechercheTab (){
-
+function RechercheTab(){
+    let rang = parseInt(prompt("Choisir rang du tableau"));
+    document.getElementById("tableauPhrase2").innerHTML = ("<br> La valeur du tableau au rang " + rang + " est de : " + tableau2[(rang-1)]);
 }
 
-function InfoTab (){
+function InfoTab(){
+    let somme = 0;
+    for(i=0; i<tableau2.length; i++){
+        somme += parseInt(tableau2[i]);
+        document.getElementById("tableauPhrase2").innerHTML = ("<br> La somme de toutes les valeurs saisies est de : " + somme);
+    }
 
+    let moyenne = 0;
+    moyenne = somme / tableau2.length;
+    document.getElementById("tableauPhrase2").innerHTML = ("<br> La somme de toutes les valeurs saisies est de : " + somme + 
+    "<br> La moyenne du tableau est de : " + moyenne);
 }
 
-GetInteger();
-InitTab();
-SaisieTab();
-AfficheTab();
-RechercheTab();
-InfoTab();
+// InitTab();
+// SaisieTab();
+// AfficheTab();
+// RechercheTab();
+// InfoTab();
